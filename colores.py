9b9f1b3ee25d9923bdae5dc1b9c23f8e1864d83e
pauladiaz-sketch <paula.diaz@galileo.edu>
2026-06-06 23:15:24 -0600

    Add files via upload

---
+++ colores.py
@@ -0,0 +1,50 @@
+GRID_COLOR="#a39489"
+EMPTY_CELL_COLOR="#c2b3a9"
+SCORE_LABEL_FONT=("verdana", 24)
+SCORE_FONT=("Helvetica", 36, "bold")
+GAME_OVER_FONT=("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR="#ffffff"
+WINNER_BG="#ffcc00"
+LOSER_BG="#a39489"
+
+CELL_COLORS={
+    2: "#C4FA89",
+    4:"#A3F244",
+    8:"#93F022",
+    16:"#86EB0C",
+    32:"#77D406",
+    64:"#6EBD0D",
+    128:"#5DA10A",
+    256:"#71AB2B",
+    512:"#528519",
+    1024:"#448507",
+    2048:"#356903",
+    
+}
+
+CELL_NUMBER_COLORS={ 
+    2: "#39472D",
+    4: "#39472D",  
+    8: "#39472D",  
+    16: "#39472D",  
+    32: "#39472D",  
+    64: "#39472D",  
+    128: "#39472D",     
+    256: "#39472D",  
+    512: "#39472D",  
+    1024: "#39472D",  
+    2048: "#39472D",}
+
+CELL_NUMBER_FONTS={
+    2:("Helvetica",50,"bold"),
+    4:("Helvetica",50,"bold"),
+    8:("Helvetica",50,"bold"),
+    16:("Helvetica",50,"bold"),
+    32:("Helvetica",50,"bold"),
+    64:("Helvetica",50,"bold"),
+    128:("Helvetica",50,"bold"),
+    256:("Helvetica",50,"bold"),
+    512:("Helvetica",50,"bold"),
+    1024:("Helvetica",50,"bold"),
+    2048:("Helvetica",50,"bold"),
+}
